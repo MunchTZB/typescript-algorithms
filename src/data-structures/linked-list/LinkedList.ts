89f59792feba60706ad2c2dcddc032dfc83ea329
汤振邦 <tangzhenbang@bytedance.com>
2018-12-25 21:26:13 +0800

    update

---
+++ src/data-structures/linked-list/LinkedList.ts
@@ -0,0 +1,189 @@
+import LinkedListNode from './LinkedListNode';
+import Comparator from '../../utils/Comparator';
+
+interface FindParams {
+  value: any;
+  callback: Function;
+}
+
+export default class LinkedList {
+  head: LinkedListNode;
+  tail: LinkedListNode;
+  compare: Comparator;
+
+  constructor(comparatorFunction: Function) {
+    this.head = null;
+    this.tail = null;
+
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  prepend(value: any): LinkedList {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  append(value: any): LinkedList {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail = newNode;
+
+    return this;
+  }
+
+  delete(value: any): LinkedListNode {
+    if (!this.head) {
+      return null;
+    }
+
+    let deleteNode = null;
+
+    while (this.head && this.compare.equal(this.head.value, value)) {
+      deleteNode = this.head;
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (this.compare.equal(currentNode.next.value, value)) {
+          deleteNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+
+    if (this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode;
+    }
+
+    return deleteNode;
+  }
+
+  find(findParams: FindParams) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      // If callback is specified then try to find node by callback.
+      if (findParams.callback && findParams.callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      // If value is specified then try to compare by value..
+      if (findParams.value !== undefined && this.compare.equal(currentNode.value, findParams.value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  deleteTail(): LinkedListNode {
+    const deletedTail = this.tail;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  deleteHead(): LinkedListNode {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+
+  fromArray(values: Array<any>) {
+    values.forEach(element => this.append(element));
+
+    return this;
+  }
+
+  toArray(): Array<LinkedListNode> {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  toString(callback: Function):string {
+    return this.toArray().map(node => node.toString(callback)).toString();
+  }
+
+  reverse():LinkedList {
+    let currNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currNode) {
+      // Store next node.
+      nextNode = currNode.next;
+
+      // Change next node of the current node so it would link to previous node.
+      currNode.next = prevNode;
+
+      // Move prevNode and currNode nodes one step forward.
+      prevNode = currNode;
+      currNode = nextNode;1
+    }
+
+    // Reset head and tail.
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
+}
---
+++ src/data-structures/linked-list/LinkedListNode.ts
@@ -0,0 +1,12 @@
+export default class LinkedListNode {
+  value: any;
+  next: LinkedListNode;
+  constructor(value: any, next: LinkedListNode = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  toString(callback: Function) {
+    return callback ? callback(this.value) : `${this.value}`;
+  }
+}
