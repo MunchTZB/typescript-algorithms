0195c0a48795620785d23fd94d0152f21b6730a0
Munch_TZB <www.tzb@gmail.com>
2019-01-14 23:24:45 +0800

    finish heap

--- src/data-structures/heap/Heap.ts
+++ src/data-structures/heap/Heap.ts
@@ -4,7 +4,7 @@ export default class Heap {
   heapContainer: Array<any>;
   compare: Comparator;
 
-  constructor(comparatorFunction: Function) {
+  constructor(comparatorFunction?: Function) {
     if (new.target === Heap) {
       throw new TypeError('Cannot construct Heap instance directly');
     }
@@ -75,9 +75,84 @@ export default class Heap {
     const item = this.heapContainer[0];
 
     this.heapContainer[0] = this.heapContainer.pop();
+    this.heapifyDown();
 
+    return item;
   }
 
+  add(item: any): Heap {
+    this.heapContainer.push(item);
+    this.heapifyUp();
+    return this;
+  }
+
+  remove(item: any, comparator: Comparator = this.compare): Heap {
+    const numberOfItemsToRemove = this.find(item, comparator).length;
+
+    for (let iteration = 0; iteration < numberOfItemsToRemove; iteration++) {
+      const indexToRemove = this.find(item, comparator).pop();
+
+      if (indexToRemove === (this.heapContainer.length - 1)) {
+        this.heapContainer.pop();
+      } else {
+        this.heapContainer[indexToRemove] = this.heapContainer.pop();
+
+        const parentItem = this.parent(indexToRemove);
+
+        // If there is no parent or parent is in correct order with the node
+        // we're going to delete then heapify down. Otherwise heapify up.
+        // 这块儿不是特别明白 翻回来看一下
+        if (
+          this.hasLeftChild(indexToRemove)
+          && (
+            !parentItem
+            || this.pairIsInCorrectOrder(parentItem, this.heapContainer[indexToRemove])
+          )
+        ) {
+          this.heapifyDown(indexToRemove);
+        } else {
+          this.heapifyUp(indexToRemove);
+        }
+      }
+    }
+
+    return this;
+  }
+
+  find(item: any, comparator: Comparator = this.compare): Array<number> {
+    const foundItemIndices = [];
+
+    for (let itemIndex = 0; itemIndex < this.heapContainer.length; itemIndex++) {
+      if (comparator.equal(item, this.heapContainer[itemIndex])) {
+        foundItemIndices.push(itemIndex);
+      }
+    }
+
+    return foundItemIndices;
+  }
+
+  isEmpty() {
+    return !this.heapContainer.length;
+  }
+
+  toString() {
+    return this.heapContainer.toString();
+  }
+
+  // 上浮操作
+  heapifyUp(customStartIndex: number = this.heapContainer.length - 1) {
+    let currentIndex = customStartIndex
+
+    while (
+      this.hasParent(currentIndex)
+      && !this.pairIsInCorrectOrder(this.parent(currentIndex), this.heapContainer[currentIndex])
+    ) {
+      this.swap(currentIndex, this.getParentIndex(currentIndex));
+      currentIndex = this.getParentIndex(currentIndex);
+    }
+  }
+
+  // 下沉操作
   heapifyDown(customStartIndex: number = 0) {
     let currentIndex = customStartIndex;
     let nextIndex = null;
@@ -91,9 +166,17 @@ export default class Heap {
       } else {
         nextIndex = this.getLeftChildIndex(currentIndex);
       }
-    }
 
-    
+      if (this.pairIsInCorrectOrder(
+        this.heapContainer[currentIndex],
+        this.heapContainer[nextIndex]
+      )) {
+        break;
+      }
+
+      this.swap(currentIndex, nextIndex);
+      currentIndex = nextIndex;
+    }
   }
 
   pairIsInCorrectOrder(firstElement: any, secondElement: any): boolean {
