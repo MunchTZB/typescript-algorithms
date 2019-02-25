957fdd5c085e417a4c05dfd8926ec86ace1d5c43
汤振邦 <tangzhenbang@bytedance.com>
2019-02-25 21:41:07 +0800

    disjointsetitem

---
+++ src/data-structures/disjoint-set/DisjointSetItem.ts
@@ -0,0 +1,66 @@
+export default class DisjointSetItem {
+  value: any;
+  keyCallback: Function;
+  parent: null | DisjointSetItem;
+  children: Map<any, DisjointSetItem>;
+
+  constructor(value: any, keyCallback: Function) {
+    this.value = value;
+    this.keyCallback = keyCallback
+  }
+
+  getKey():any {
+    if (this.keyCallback) {
+      return this.keyCallback(this.value);
+    }
+
+    return this.value;
+  }
+
+  getRoot(): DisjointSetItem {
+    return this.isRoot() ? this : this.parent.getRoot();
+  }
+
+  isRoot(): boolean {
+    return this.parent === null;
+  }
+
+  getRank(): number {
+    if (this.children.size === 0) {
+      return 0;
+    }
+
+    let rank = 0;
+
+    this.getChildren().forEach(child => {
+      rank += 1;
+      
+      rank += child.getRank();
+    });
+
+    return rank;
+  }
+
+  getChildren(): Array<DisjointSetItem> {
+    return [...this.children.values()];
+  }
+
+  setParent(
+    parentItem: DisjointSetItem,
+    forceSettingParentChild: boolean
+  ): DisjointSetItem {
+    this.parent = parentItem;
+    if (forceSettingParentChild) {
+      parentItem.addChild(this);
+    }
+
+    return this;
+  }
+
+  addChild(childItem: DisjointSetItem): DisjointSetItem {
+    this.children.set(childItem.getKey(), childItem);
+    childItem.setParent(this, false);
+
+    return this;
+  }
+}
